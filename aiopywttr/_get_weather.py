f73e4ef921bd28e8bf1c0238d308195af7974ef4
monosans <hsyqixco@protonmail.com>
2024-01-20 08:28:17 +0000

    Update .pre-commit-config.yaml (#465)
    * Update .pre-commit-config.yaml
    
    * Fix
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- aiopywttr/_get_weather.py
+++ aiopywttr/_get_weather.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 import pywttr_models
 from aiohttp import ClientSession
-from pywttr_models._language import Language
+from pywttr_models._language import Language  # noqa: PLC2701
 from typing_extensions import Literal, overload
 
 from ._http import get_json
